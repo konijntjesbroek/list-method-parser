a2ee3e50401c2aa071bf3fa79abe7d2b091a47a9
konijntjesbroek <proticon@gmail.com>
2020-12-03 00:35:49 -0600

    cleanup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 01234567890123456789012345678901234567890123456789012345678901234567890123456789
-          1         2         3         4         5         6         7         
+          1         2         3         4         5         6         7
 List Method Parser
 Author: Arlo Gittings
 Created: 2020-11-20
@@ -11,66 +11,76 @@ Usage:
                                     i for interactive
                                     q to quit
 
-         <number>:                  enter a command from the below methods format:
-                                        command arg1 arg2 ...
+         <number>:                  enter a command from the below methods
+                                    format: command arg1 arg2 ...
                                     ex:
-                                        append 'new_element' 
-                                    
-                                    If interective mode is chosen commands may be
-                                    entered one at a time, after which the array 
-                                    and any returns will be printed to the console
+                                        append 'new_element'
 
-Available methods:    
+                                    If interective mode is chosen commands may
+                                    be entered one at a time, after which the
+                                    array and any returns will be printed to
+                                    the console
+
+Available methods:
     append(new_element)             add new_element to end of list
     clear()                         remove all elements from the list
-    count(element)                  returns the number of occurances of element in
-                                        list
+    count(element)                  returns the number of occurances of element
+                                    in list.
     extend(new_list)                add new_list to the end of list
-    index(element)                  return the first location in the list where 
-                                        element is found or throw a ValueError 
+    index(element)                  return the first location in the list where
+                                    element is found or throw a ValueError
     insert(index, new_element)      add new_element at index
-    pop(<index>)                    remove an element from the list and return its
-                                        value, if index is not provided the last 
-                                        element will be popped
+    pop(<index>)                    remove an element from the list and return
+                                    its value, if index is not provided the
+                                    last element will be popped
     print                           display the current state of the array
     remove(element)                 remove the first occurance of element
-    reverse()                       place all elements of list in opposite order 
-    sort(key, reverse=True|False)   sort the list according to function key with 
-                                        largest or smallest value first
+    reverse()                       place all elements of list in opposite
+                                    order
+    sort(key, reverse=True|False)   sort the list according to function key
+                                    with largest or smallest value first
 
 Special considerations:
     Strings must be encapsulate by quotes:
-        append 'element'            <= works ok 
+        append 'element'            <= works ok
         append element              <= not so much
-        
-    Spaces are currently reserved for separating arguments, so placing spaces insde of an argument will cause this to throw mysterious errors. For example:
-        
-        extend [1,2,3,4,5]          <= works ok 
-        extend [1, 2, 3, 4, 5]      <= not so much
-        
-        append 'new_element'        <= works ok 
-        append 'new element'        <= breaks in a funny, non-obvious (to me) manner
-    
-    Methods that return a value are functional but the return is thrown a way this causes pop() to look suspiciously like it just removed the last element. index() does not appear to do anything, yet. This is top on my resolution bucket.
-    
 
-          1         2         3         4         5         6         7         
+    Spaces are currently reserved for separating arguments, so placing spaces
+    insde of an argument will cause this to throw mysterious errors.
+    For example:
+
+        extend [1,2,3,4,5]          <= works ok
+        extend [1, 2, 3, 4, 5]      <= not so much
+
+        append 'new_element'        <= works ok
+        append 'new element'        <= breaks in a funny, non-obvious manner
+
+    Methods that return a value are functional but the return is thrown a way
+    this causes pop() to look suspiciously like it just removed the last
+    element. index() does not appear to do anything, yet. This is top on my
+    resolution bucket.
+
+          1         2         3         4         5         6         7
 01234567890123456789012345678901234567890123456789012345678901234567890123456789
 '''
-def welcome():
-    greetings='''Welcome to the list method toy!
 
-    This tool has been created to familiarize myself with the list methods 
-    available in python 3. Originally it was created to solve a simple challenge 
-    on HackerRank. Then it sorta took over if you need anything, feel free to 
-    drop me a message
+
+def welcome():
+
+    greetings = '''Welcome to the list method toy!
+
+    This tool has been created to familiarize myself with the list methods
+    available in python 3. Originally it was created to solve a simple
+    challenge on HackerRank. Then it sorta took over if you need anything, feel
+    free to drop me a message
     '''
     print(greetings)
-                     
+
+
 def get_input():
+    prompt = 'b for batch\ni for interactive\nq to quit\n\nSelect Mode: '
     while True:
-        input_mode = input('b for batch\ni for interactive\nq to quit\n\nSelect Mode: ').strip()
-    
+        input_mode = input(prompt).strip()
         if input_mode.isnumeric():
             return input_mode
         elif input_mode.lower() == 'b':
@@ -82,6 +92,7 @@ def get_input():
         else:
             print('invalid command')
 
+
 def b_mode(command_count):
     arr = []
     commands = []
@@ -90,21 +101,25 @@ def b_mode(command_count):
         command_count = input('Enter the number of commands to run: ').strip()
     command_count = int(command_count)
 
-    for c in range(command_count): 
+    for c in range(command_count):
         width = 1
         while command_count >= 10 ** width:
-            width += 1 
-        commands.append(input((' ' * (5 - width)) + 'command ' + str(c+1).rjust(width, ' ') + ': ').strip().split()) 
+            width += 1
+        commands.append(input((' ' * (5 - width)) + 'command ' +
+                              str(c+1).rjust(width, ' ') +
+                              ': ').strip().split())
     for cmd in commands:
         if cmd[0].lower() == 'print':
-            print(f'holder: {holder}\narr: {arr}\n') 
+            print(f'holder: {holder}\narr: {arr}\n')
         elif cmd[0].lower() == 'pop' or cmd[0].lower() == 'count':
             holder = eval('arr.'+cmd[0]+'('+', '.join(cmd[1:])+')')
         else:
             eval('arr.'+cmd[0]+'('+', '.join(cmd[1:])+')')
 
+
 def i_mode():
     print('Entering interactive mode')
+
 
 welcome()
 parse_mode = get_input()
@@ -114,5 +129,5 @@ while parse_mode:
         i_mode()
     else:
         b_mode(parse_mode)
-    
+
     parse_mode = get_input()
